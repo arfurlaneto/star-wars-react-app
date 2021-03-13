e6a7de41fd5ced578239c7cba245bebb6fcc2926
arfurlaneto <arfurlaneto@gmail.com>
2021-03-12 22:00:38 -0300

    Using styled-components global styles.

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,51 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
 import PaginatedList from './components/PaginatedList';
 
+import GlobalStyles from './globalStyles';
+
 import {
   MenuContainer,
   MenuItemText
 } from './styles'
 
-const endpoints = [
-  "people",
-  "films",
-  "starships",
-  "vehicles",
-  "species",
-  "planets",
-]
-
 function App() {
   return (
-    <div>
       <BrowserRouter>
+        <GlobalStyles></GlobalStyles>
+        
         <MenuContainer>
-          {endpoints.map(endpoint =>
-            <MenuItemText key={endpoint}>
-              <Link to={endpoint}>
-                {endpoint}
-              </Link>
-            </MenuItemText>
-          )}
+          <MenuItemText><Link to="people">people</Link></MenuItemText>
+          <MenuItemText><Link to="films">films</Link></MenuItemText>
+          <MenuItemText><Link to="starships">starships</Link></MenuItemText>
+          <MenuItemText><Link to="vehicles">vehicles</Link></MenuItemText>
+          <MenuItemText><Link to="species">species</Link></MenuItemText>
+          <MenuItemText><Link to="planets">planets</Link></MenuItemText>
         </MenuContainer>
 
         <Switch>
           <Route key="/" path="/" exact>
-            <Redirect to={`/${endpoints[0]}`} />
+            <Redirect to="/people" />
           </Route>
-          {endpoints.map(endpoint =>
-            <Route key={endpoint} path={`/${endpoint}`}>
-              <PaginatedList endpoint={endpoint}></PaginatedList>
+          <Route path="/people">
+            <PaginatedList endpoint="people" />
             </Route>
-            )}
+          <Route path="/films">
+            <PaginatedList endpoint="films" />
+            </Route>
+          <Route path="/starships">
+            <PaginatedList endpoint="starships" />
+            </Route>
+          <Route path="/vehicles">
+            <PaginatedList endpoint="vehicles" />
+            </Route>
+          <Route path="/species">
+            <PaginatedList endpoint="species" />
+            </Route>
+          <Route path="/planets">
+            <PaginatedList endpoint="planets" />
+          </Route>
         </Switch>
       </BrowserRouter>
-    </div>
   );
 }
 
